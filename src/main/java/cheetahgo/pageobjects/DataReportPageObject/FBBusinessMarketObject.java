9a827aba3757ee73f6b5fcf989c1341b88a2e355
hmz0913 <huangmingzhu@cmcm.com>
2023-10-16 17:42:50 +0800

    2023-10-16 17:42

---
+++ src/main/java/cheetahgo/pageobjects/DataReportPageObject/FBBusinessMarketObject.java
@@ -0,0 +1,66 @@
+package cheetahgo.pageobjects.DataReportPageObject;
+
+import cheetahgo.constant.Constants;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import util.PropertiesMapUtil;
+
+public class FBBusinessMarketObject {
+    private WebElement element = null;
+    private PropertiesMapUtil objectMap = new PropertiesMapUtil(Constants.CooperativeManagement);
+    private WebDriver webDriver;
+
+    public FBBusinessMarketObject(WebDriver webDriver) throws Exception {
+        this.webDriver = webDriver;
+    }
+    //消耗大盘-日期-开始
+    public WebElement dateStart() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.dateStart"));
+        return element;
+    }
+    //消耗大盘-日期-结束
+    public WebElement dateEnd() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.dateEnd"));
+        return element;
+    }
+    //消耗大盘-搜索
+    public WebElement search() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.Search"));
+        return element;
+    }
+    //违规创意-日期-开始
+    public WebElement illegalCreativityDateStart() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.illegalCreativityDateStart"));
+        return element;
+    }
+    //违规创意-日期-结束
+    public WebElement illegalCreativityDateEnd() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.illegalCreativityDateEnd"));
+        return element;
+    }
+    //违规创意-日期-结束
+    public WebElement illegalCreativityDateEnd() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.illegalCreativityDateEnd"));
+        return element;
+    }
+    //违规创意-搜索
+    public WebElement illegalCreativitySearch() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.illegalCreativitySearch"));
+        return element;
+    }
+    //开户大盘-日期-开始
+    public WebElement accountOpeningMarketDateStart() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.accountOpeningMarketDateStart"));
+        return element;
+    }
+    //开户大盘-日期-结束
+    public WebElement accountOpeningMarketDateEnd() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.accountOpeningMarketDateEnd"));
+        return element;
+    }
+    //开户大盘-搜索
+    public WebElement accountOpeningMarketSearch() throws Exception {
+        element = webDriver.findElement(objectMap.getLocator("cheetahGo.businessManagement.fbBusinessMarket.accountOpeningMarketSearch"));
+        return element;
+    }
+}
